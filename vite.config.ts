b7a8a9a33afab2c6b3ba5cc2a13ed64a0a357cb1
transportians-devteam <trans.edteam@gmail.com>
2026-05-16 15:19:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
 
-  base: "/aggregetor", 
+  base: "/aggregetor/", 
   server: {
     host: "::",
     port: 3000,
